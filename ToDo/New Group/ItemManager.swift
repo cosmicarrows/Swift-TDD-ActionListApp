4449dfcf97079020fc02fcfba720d020a5cdfad8
Laurence Wingo <laurencewingo@laurences-air.gsu.edu>
2018-05-16 16:47:39 -0400

    Implemented Unit Tests for ItemManager toDoCount and doneCount

---
+++ ToDo/New Group/ItemManager.swift
@@ -0,0 +1,14 @@
+//
+//  ItemManager.swift
+//  ToDo
+//
+//  Created by Laurence Wingo on 5/16/18.
+//  Copyright © 2018 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import Foundation
+
+class ItemManager: NSObject {
+    let toDoCount = 0
+    let doneCount = 0
+}
--- ToDoTests/Model Tests/ItemManagerTests.swift
+++ ToDoTests/Model Tests/ItemManagerTests.swift
@@ -7,16 +7,29 @@
 //
 
 import XCTest
+@testable import ToDo
 
 class ItemManagerTests: XCTestCase {
     
+    var itemManager: ItemManager!
+    
     override func setUp() {
         super.setUp()
+        itemManager = ItemManager.init()
     }
     
     override func tearDown() {
         super.tearDown()
     }
+    
+    func test_ToDoCount_InitiallyIsZero(){
+        XCTAssertEqual(itemManager.toDoCount, 0)
+    }
+    
+    func test_DoneCount_InitiallyIsZero(){
+        XCTAssertEqual(itemManager.doneCount, 0)
+    }
+    
     
    
     
